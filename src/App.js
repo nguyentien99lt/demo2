722cda639821a162c8187745a596176d738cb710
tiennv <nguyenvantienn99@gmail.com>
2021-06-30 13:43:45 +0700

    moi day thoi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ok nhé người ae.
+          Edit <code>src/App.js</code> and save to reload ok nhé người ae lalaaa.
         </p>
         <a
           className="App-link"
