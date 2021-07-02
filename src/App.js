d13c6a14e682e6f401f2956c2a3cfed516dc31e1
tiennv <nguyenvantienn99@gmail.com>
2021-07-02 21:03:26 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import Header from "./components/admin/header";
 import Main from "./components/admin/main";
 import Nav from "./components/admin/nav";
 import "./styles.css";
+import { useState } from "react";
 
 
 export default function App() {
+
+  const [Products, setProducts] = useState([
+    { id: 1, name: "product1", price: "2000" },
+    { id: 2, name: "product2", price: "3000" },
+    { id: 3, name: "product3", price: "4000" },
+    { id: 4, name: "product4", price: "5000" }
+  ]);
+
+  const onHandleRemove = (x) => {
+    const newProduct = Products.filter((item) => item.id != x)
+    setProducts(newProduct);
+
+  };
+
   return (
     <div>
       <div>
@@ -15,7 +30,7 @@ export default function App() {
         <div className="container-fluid">
           <div className="row">
             <Nav />
-            <Main />
+            <Main data={Products} onRemove={onHandleRemove} />
           </div>
         </div>
       </div>
--- src/components/admin/main.js
+++ src/components/admin/main.js
@@ -1,44 +1,43 @@
-export default function Main() {
-    return (
-      <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 className="h2">Quản lý sản phẩm</h1>
-          <div className="btn-toolbar mb-2 mb-md-0">
-            <button type="button" className="btn btn-sm btn-outline-primary">
-              Thêm sản phẩm
-            </button>
-          </div>
+export default function Main(props) {
+
+  function ok() {
+    alert("ok");
+
+  };
+  return (
+    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 className="h2">Quản lý sản phẩm</h1>
+        <div className="btn-toolbar mb-2 mb-md-0">
+          <button type="button" className="btn btn-sm btn-outline-primary" onClick={ok}>
+            Thêm sản phẩm
+          </button>
         </div>
-        <div className="table-responsive">
-          <table className="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
-                <th scope="col">Header</th>
+      </div>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Stt</th>
+              <th scope="col">Name</th>
+              <th scope="col">Price</th>
+              <th scope="col"></th>
+
+            </tr>
+          </thead>
+          <tbody>
+
+            {props.data.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td> <button className="btn btn-danger btn-sm" onClick={() => props.onRemove(item.id)}>Delete</button></td>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1,001</td>
-                <td>random</td>
-                <td>data</td>
-                <td>placeholder</td>
-                <td>text</td>
-              </tr>
-              <tr>
-                <td>1,002</td>
-                <td>placeholder</td>
-                <td>irrelevant</td>
-                <td>visual</td>
-                <td>layout</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </main>
-    );
-  }
-  
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
+}
